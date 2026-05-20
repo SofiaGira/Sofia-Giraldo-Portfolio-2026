ac20434d8760ad45fdc58e12f9c46056a0955927
SofiaGira <sofi.giraldo.a@gmail.com>
2026-05-20 14:17:41 +0800

    nav bar upgrade
    added drop down menu, menu button
    changed shape of picture
    added movement of drop down menu

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById("menu-btn");
+
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => {
+
+  menu.classList.toggle("active");
+
+});
